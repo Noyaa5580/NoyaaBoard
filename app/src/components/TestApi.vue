6a0adae82868f388e761f18f33fd64a261a25c92
Noyaa5580 <timofeybarya2005@gmail.com>
2024-02-28 22:10:57 +0300

    check json-server in test repo for Kursach

---
+++ app/src/components/TestApi.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div v-for="item in popular" :key="item">
+      <div class="card">
+        <p>{{ item.name }}</p>
+        <p>{{ item.author }}</p>
+        <img :src="item.img_link" alt="" />
+        <p>{{ item.price }}</p>
+        <p>{{ item.description }}</p>
+      </div>
+    </div>
+  </div>
+</template>>
+    
+<style scoped>
+img {
+  width: 300px;
+  height: 400px;
+  object-fit: cover;
+}
+.card{
+    margin-bottom: 50px;
+}
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      popular: [],
+    };
+  },
+  methods: {},
+  mounted() {
+    axios
+      .get("http://localhost:3000/cathalog")
+      .then((response) => (this.popular = response.data));
+  },
+};
+</script>
